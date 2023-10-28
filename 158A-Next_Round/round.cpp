37f613a2830304456fe69bc52a8d0cb669f753cb
VipulChauhan89 <Vipulchauhan548@gmail.com>
2023-10-28 23:39:34 +0530

    Created round.cpp

---
+++ 158A-Next_Round/round.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main()
+{
+    int n,k,count=0;
+    cin>>n>>k;
+    vector<int> A(n);
+    for(int i=0;i<n;i++)
+    {
+        cin>>A[i];
+    }
+    for(int i=0;i<n;i++)
+    {
+        if(A[i]>=A[k-1] && A[i]>0)
+        {
+            count++;
+        }
+    }
+    cout<<count<<endl;
+    return 0;
+}
