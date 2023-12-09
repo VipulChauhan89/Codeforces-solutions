242092c3ce4c5327fd43a4ae91b58b02f4de4ea1
VipulChauhan89 <Vipulchauhan548@gmail.com>
2023-12-09 19:06:34 +0530

    Created bacteria.cpp

---
+++ 579A-Raising_Bacteria/bacteria.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    long long x,count=0;
+    cin>>x;
+    while(x/2!=0)
+    {
+        if(x%2==1)
+        {
+            count++;
+        }
+        x/=2;
+    }
+    cout<<count+1<<endl;
+    return 0;
+}
