1aab3d0daabd75b0bd4edd7a5fac13e85e286abf
VipulChauhan89 <Vipulchauhan548@gmail.com>
2024-01-02 09:55:03 +0530

    Created company.cpp

---
+++ 935A-Fafa_and_his_Company/company.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n,count=1;
+    cin>>n;
+    for(int i=2;i<=n/2;i++)
+    {
+        if(n%i==0)
+        {
+            count++;
+        }
+    }
+    cout<<count<<endl;
+    return 0;
+}
