7806d063acb6b5d95ffea40245217cf8d29ff28d
VipulChauhan89 <Vipulchauhan548@gmail.com>
2024-02-03 12:44:22 +0530

    Created triangle.cpp

---
+++ 1337A-Ichihime_and_Triangle/triangle.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int t,a,b,c,d;
+    cin>>t;
+    while(t--)
+    {
+        cin>>a>>b>>c>>d;
+        cout<<endl<<b<<" "<<c<<" "<<c<<endl;
+    }
+    return 0;
+}
