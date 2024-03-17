d352bc3abe45284ff01bb5133f7ffabb7b3734e1
VipulChauhan89 <Vipulchauhan548@gmail.com>
2024-03-17 22:13:05 +0530

    Created gregor.cpp

---
+++ 1549A-Gregor_and_Cryptography/gregor.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int t,p;
+    cin>>t;
+    while(t--)
+    {
+        cin>>p;
+        cout<<(p-1)/2<<" "<<(p-1)<<endl;
+    }
+    return 0;
+}
